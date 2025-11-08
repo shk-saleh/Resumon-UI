171df0d6b405c8b1b40dbdb5a55c0baa1c9e3e6a
gptone-me <sanabano.gptone@gmail.com>
2025-11-08 22:10:44 +0500

    zustand setup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -44,7 +44,7 @@ const Signup = ({ setMode }) => {
         <label id="password" htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2"> Password </label>
           <input
             type="password" id="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleChange} required
-            className="w-full px-4 py-2.5 text-(--gray-color) border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#1A9B7F] focus:border-transparent transition"
+            className="w-full px-4 py-2.5 text-(--gray-color) rounded-lg border border-gray-300 focus:outline-none focus:ring-1 focus:ring-[#1A9B7F] focus:border-transparent transition"
           />
       </div>
 

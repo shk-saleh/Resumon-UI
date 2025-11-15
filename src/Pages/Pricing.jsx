fcd9df0205cd88891e043883b46c2dd720fbf21a
gptone-me <sanabano.gptone@gmail.com>
2025-11-15 13:02:34 +0500

    fixed the page blob affect

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -5,13 +5,14 @@ import Footer from "../components/Footer";
 import PricingPlans from "../components/PricingPlans";
 import Comparison from "../components/Comparison";
 import CustomPLans from "../components/CustomPlans";
+import blob1 from '/src/assets/images/blob1.png';
 
 const Pricing = () => {
   return (
     <div className="min-h-screen bg-white relative">
  
       <Navbar />
-      <img src="/src/assets/images/blob1.png" alt="background blob"
+      <img src={blob1} alt="background blob"
         className="absolute top-0 left-0 w-[350px] sm:w-[500px] md:w-[800px] lg:w-[900px] opacity-90 z-10 pointer-events-none select-none"
       />
 

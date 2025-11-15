e754d7694a4dfdd8aab26243ff53cd96b7ac005a
gptone-me <sanabano.gptone@gmail.com>
2025-11-15 13:13:33 +0500

    dashboard recoverd

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '../store/useAuthStore';
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
+import MainContent from "../components/MainContent";
 
 const Dashboard = () => {
-
-  const { logout } = useAuthStore();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate('/get-started');
-  }
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   return (
-    <div className="min-h-screen bg-white">
-        <Navbar />
-        <div className='flex justify-center items-center h-100'>
-          <button onClick={handleLogout} className='bg-gray-500 p-4 rounded-2xl cursor-pointer'>Logout</button>
-        </div>
-        <Footer />
+    <div className="flex h-screen bg-gradient-to-r from-[#FFFFFF] to-[#F1FFFB]">
+      <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+
+      <div className="flex-1 flex flex-col">
+
+        <Topbar/>
+        <MainContent/>
+        
+      </div>
     </div>
   );
 };
